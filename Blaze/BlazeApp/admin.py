9070436a12306c450ec0c0a48279a7b9ff446f25
Dinesh <dhanjeehartik@gmail.com>
2023-10-16 23:50:50 +0500

    Removed all bad files.

--- Blaze/BlazeApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Student, Faculty, SocietyPage
-
-# Register your models here.
-admin.site.register(Student)
-# admin.site.register(Faculty)
-# admin.site.register(SocietyPage)
