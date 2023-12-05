fc72a4c48f1d57fbd66b20a433b22c0dd079d483
Dinesh <dhanjeehartik@gmail.com>
2023-12-05 15:27:51 +0500

    Added static root

--- Blaze/Blaze/settings.py
+++ Blaze/Blaze/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "allstatic/"
+STATIC_ROOT = BASE_DIR / "productionfiles/"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
